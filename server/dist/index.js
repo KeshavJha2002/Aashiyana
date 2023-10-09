59079a50f695a85a4d753c119165bd3dd498d7d8
Keshav Jha <jhak78022@gmail.com>
2023-10-09 22:22:18 +0530

    connected to database

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+mongoose_1.default.connect(process.env.DB)
+    .then(() => console.log("connected to db"))
+    .catch((e) => console.log(e));
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server is being hosted in ${process.env.PORT || 4000}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World")
+import express,{Request,Response} from 'express'; 
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.DB!)
+  .then(()=>console.log("connected to db"))
+  .catch((e:Error)=>console.log(e));
+
+app.get('/',(req:Request,res:Response)=>{
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT || 4000,()=>{
+  console.log(`Server is being hosted in ${process.env.PORT || 4000}`)
+})  
